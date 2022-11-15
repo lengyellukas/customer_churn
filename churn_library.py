528501a02bfc9e9eeada3dd07af9767ffa651f5c
lengyelluk <lengyel.luk@gmail.com>
2022-11-15 22:08:35 +0100

    minor changes

--- churn_library.py
+++ churn_library.py
@@ -1,16 +1,23 @@
-# library doc string
-""" This is a simple ML model to predict bank customer churn using Random Forest Classifer
-and Logistic Regression """
+'''
+This is a simple ML model to predict bank customer churn using Random Forest Classifer
+and Logistic Regression
+
+Author: Lukas Lengyel
+
+Date: 15.11.2022
+'''
+
 
 # import libraries
 import os
-import joblib
 import logging
+import joblib
 import matplotlib
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
 from sklearn.metrics import plot_roc_curve, classification_report
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
@@ -22,12 +29,13 @@ os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 matplotlib.use('Agg')
 sns.set()
 
-#basic logging configuration
+# basic logging configuration
 logging.basicConfig(
-    filename='./results.log',
+    filename='./logs/results.log',
     level=logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
+
 
 def import_data(pth):
     '''
@@ -61,41 +69,46 @@ def perform_eda(dataframe):
         lambda val: 0 if val == "Existing Customer" else 1)
 
     # create histogram of customer churn
-    plt.figure(figsize=(20,10))
+    plt.figure(figsize=(20, 10))
     dataframe_eda['Churn'].hist()
     logging.info("Customer Churn histogram created")
     plt.savefig(fname='./images/eda/customer_churn_histogram.png')
     plt.close()
 
     # create histogram of customer age
-    plt.figure(figsize=(20,10))
+    plt.figure(figsize=(20, 10))
     dataframe_eda['Customer_Age'].hist()
     logging.info("Customer Age histogram created")
     plt.savefig(fname='./images/eda/customer_age_histogram.png')
     plt.close()
 
     # create bar chart of customer marital status
-    plt.figure(figsize=(20,10))
+    plt.figure(figsize=(20, 10))
     dataframe_eda.Marital_Status.value_counts('normalize').plot(kind='bar')
     logging.info("Maritual Status Bar chart created")
     plt.savefig(fname='./images/eda/marital_status_bar_chart.png')
     plt.close()
 
     # create transaction density histoplot
-    plt.figure(figsize=(20,10))
+    plt.figure(figsize=(20, 10))
     sns.histplot(dataframe_eda['Total_Trans_Ct'], stat='density', kde=True)
     logging.info("Transaction Density Histogram created")
     plt.savefig(fname='./images/eda/transaction_density_histogram.png')
     plt.close()
 
     # create fancy heatmap
-    plt.figure(figsize=(20,10))
-    sns.heatmap(dataframe_eda.corr(), annot=False, cmap='Dark2_r', linewidths=2)
+    plt.figure(figsize=(20, 10))
+    sns.heatmap(
+        dataframe_eda.corr(),
+        annot=False,
+        cmap='Dark2_r',
+        linewidths=2)
     logging.info("Heatmap created")
     plt.savefig(fname='./images/eda/heatmap.png')
     plt.close()
 
     return dataframe_eda
+
 
 def encoder_helper(dataframe, category_list, response):
     '''
@@ -114,20 +127,21 @@ def encoder_helper(dataframe, category_list, response):
     # create copy of dataframe
     dataframe_encode = dataframe.copy(deep=True)
 
-    logging.info("turning each categorical column into a new column with propotion of churn")
+    logging.info(
+        "turning each categorical column into a new column with propotion of churn")
     for category in category_list:
         temp_list = []
         temp_groups = dataframe.groupby(category).mean()['Churn']
-        
+
         for cat in dataframe[category]:
-            logging.info("Converting categorical column %s", cat)    
+            logging.info("Converting categorical column %s", cat)
             temp_list.append(temp_groups.loc[cat])
 
         if response:
             new_column = category + "_" + response
             logging.info("Creating new column %s", new_column)
             dataframe_encode[new_column] = temp_list
-        
+
         else:
             dataframe_encode[category] = temp_list
 
@@ -214,42 +228,46 @@ def classification_report_image(y_train,
              None
     '''
     logging.info("Creating classification reports and storing them into files")
-    #Random Forest Classifier
-    plt.rc('figure', figsize=(6, 6))
+    # Random Forest Classifier
+    plt.rc('figure', figsize=(6, 20))
     plt.text(0.01, 1.25,
-            str('Random Forest Train'),
-            {'fontsize': 10}, fontproperties='monospace')
+             str('Random Forest Train'),
+             {'fontsize': 10}, fontproperties='monospace')
     plt.text(0.01, 0.05,
-            str(classification_report(y_test, y_test_preds_rf)),
-            {'fontsize': 10}, fontproperties='monospace')
+             str(classification_report(y_test, y_test_preds_rf)),
+             {'fontsize': 10}, fontproperties='monospace')
     plt.text(0.01, 0.6,
-            str('Random Forest Test'),
-            {'fontsize': 10}, fontproperties='monospace')
+             str('Random Forest Test'),
+             {'fontsize': 10}, fontproperties='monospace')
     plt.text(0.01, 0.7,
-            str(classification_report(y_train, y_train_preds_rf)),
-            {'fontsize': 10}, fontproperties='monospace')
+             str(classification_report(y_train, y_train_preds_rf)),
+             {'fontsize': 10}, fontproperties='monospace')
     plt.axis('off')
     plt.savefig(fname='./images/results/random_forest_results.png')
-    logging.info("The result has been stored in the file %s", "random_forest_results.png")
+    logging.info(
+        "The result has been stored in the file %s",
+        "random_forest_results.png")
     plt.close()
 
     # Logistic Regression
-    plt.rc('figure', figsize=(6, 6))
+    plt.rc('figure', figsize=(6, 20))
     plt.text(0.01, 1.25,
-            str('Logistic Regression Train'),
-            {'fontsize': 10}, fontproperties='monospace')
+             str('Logistic Regression Train'),
+             {'fontsize': 10}, fontproperties='monospace')
     plt.text(0.01, 0.05,
-            str(classification_report(y_train, y_train_preds_lr)),
-            {'fontsize': 10}, fontproperties='monospace')
+             str(classification_report(y_train, y_train_preds_lr)),
+             {'fontsize': 10}, fontproperties='monospace')
     plt.text(0.01, 0.6,
-    str('Logistic Regression Test'),
-            {'fontsize': 10}, fontproperties='monospace')
+             str('Logistic Regression Test'),
+             {'fontsize': 10}, fontproperties='monospace')
     plt.text(0.01, 0.7,
-            str(classification_report(y_test, y_test_preds_lr)),
-            {'fontsize': 10}, fontproperties='monospace')
+             str(classification_report(y_test, y_test_preds_lr)),
+             {'fontsize': 10}, fontproperties='monospace')
     plt.axis('off')
     plt.savefig(fname='./images/results/logistic_results.png')
-    logging.info("The result has been stored in the file %s", "logistic_results.png")
+    logging.info(
+        "The result has been stored in the file %s",
+        "logistic_results.png")
     plt.close()
 
 
@@ -295,8 +313,8 @@ def feature_importance_plot(model, X_data, output_path):
         '.' +
         file_name_suffix)
     plt.savefig(full_path)
-    logging.info("Feature importances were stored in the file %s.%s", 
-                                feature_file_name, file_name_suffix)
+    logging.info("Feature importances were stored in the file %s.%s",
+                 feature_file_name, file_name_suffix)
 
 
 def train_models(X_train, X_test, y_train, y_test):
@@ -310,7 +328,8 @@ def train_models(X_train, X_test, y_train, y_test):
     output:
               None
     '''
-    random_forest_classifier = RandomForestClassifier(random_state=42, n_jobs=-1)
+    random_forest_classifier = RandomForestClassifier(
+        random_state=42, n_jobs=-1)
     logistic_regression = LogisticRegression(max_iter=1000, n_jobs=-1)
 
     # grid search
@@ -321,7 +340,7 @@ def train_models(X_train, X_test, y_train, y_test):
         'criterion': ['gini', 'entropy']
     }
 
-    logging.info("Calculating cross validation") 
+    logging.info("Calculating cross validation")
     cross_validation_rfc = GridSearchCV(
         estimator=random_forest_classifier,
         param_grid=param_grid,
@@ -330,15 +349,17 @@ def train_models(X_train, X_test, y_train, y_test):
 
     logistic_regression.fit(X_train, y_train)
 
-    #store model
+    # store model
     logging.info("Storing the models")
     joblib.dump(cross_validation_rfc.best_estimator_, './models/rfc_model.pkl')
     joblib.dump(logistic_regression, './models/logistic_model.pkl')
     logging.info("The models were stored")
 
     # predict using random forest classifier using feature set
-    logging.info("Predicting using random forrest classifier using feature set")
-    y_train_prediction_rf = cross_validation_rfc.best_estimator_.predict(X_train)
+    logging.info(
+        "Predicting using random forrest classifier using feature set")
+    y_train_prediction_rf = cross_validation_rfc.best_estimator_.predict(
+        X_train)
     y_test_prediction_rf = cross_validation_rfc.best_estimator_.predict(X_test)
 
     # predict using logistic regression using feature set
@@ -350,9 +371,14 @@ def train_models(X_train, X_test, y_train, y_test):
     plt.figure(figsize=(15, 8))
     ax = plt.gca()
     logging.info("Calculating roc curve of logistic regression")
-    lrc_plot = plot_roc_curve(logistic_regression, X_test, y_test, ax=ax, alpha=0.8)
-    rfc_disp = plot_roc_curve(cross_validation_rfc.best_estimator_, 
-    X_test, y_test, ax=ax, alpha=0.8)
+    plot_roc_curve(
+        logistic_regression,
+        X_test,
+        y_test,
+        ax=ax,
+        alpha=0.8)
+    plot_roc_curve(cross_validation_rfc.best_estimator_,
+                   X_test, y_test, ax=ax, alpha=0.8)
     plt.savefig(fname='./images/results/roc_curve_result.png')
     plt.close()
 
